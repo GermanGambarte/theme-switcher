56f31c5d2fd32304f2b9d9e81ec7faac29ea774d
GermanGambarte <germangambarte@gmail.com>
2022-12-06 16:44:04 -0300

    feature: theme persistence

--- theme.js
+++ theme.js
@@ -3,6 +3,12 @@ const themeBtns = document.querySelectorAll(".theme");
 const handleClick = (e) => {
   const theme = e.currentTarget.dataset.theme;
   document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
 };
 
 themeBtns.forEach((t) => t.addEventListener("click", handleClick));
+
+window.addEventListener("DOMContentLoaded", () => {
+  const theme = localStorage.getItem("theme");
+  theme && document.documentElement.setAttribute("data-theme", theme);
+});
